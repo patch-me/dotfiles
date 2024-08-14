5728ceff65b24581b02ab295786d4ab00d0cf7ee
Ripitchip <thomasderudder@sfr.fr>
2024-08-14 17:19:14 +0000

    :sparkles: feat: Creation of neovim lazy configuration

---
+++ .config/nvim/lua/oh-my-nvim/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "github/copilot.vim",
+        config = function()
+            vim.g.copilot_enabled = 1
+        end
+    },
+    -- {
+    --     'Exafunction/codeium.vim',
+    --     event = 'BufEnter'
+    -- }
+}
