a4abf71854abc47343697e886d0bc5904fec291c
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-27 00:21:17 +0200

    :sparkles: feat: Full gaps update and layout + bash + picom

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 5;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -19,7 +20,9 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"st", NULL,
-    "feh","--bg-scale","/etc/images/banana.png",NULL,
+    "sh", "/etc/screen/layout.sh", NULL,
+    "feh","--scale-down", "--bg-fill", "/etc/images/banana.png", NULL,
+    "picom", "-b", NULL,
 	NULL /* terminate */
 };
 
@@ -101,6 +104,9 @@ static const Key keys[] = {
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
