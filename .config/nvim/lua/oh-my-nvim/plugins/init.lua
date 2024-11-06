8649cc2dad7a73507f8817950736151eec71ea2f
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-06 21:20:35 +0100

    :sparkles: feat: Setup vim wiki and latex

--- .config/nvim/lua/oh-my-nvim/plugins/init.lua
+++ .config/nvim/lua/oh-my-nvim/plugins/init.lua
@@ -1,34 +1,33 @@
 return {
-    --telescope
-    require("oh-my-nvim.plugins.telescope"),
+	--telescope
+	require("oh-my-nvim.plugins.telescope"),
 
-    --treesitter
-    require("oh-my-nvim.plugins.treesitter"),
-    require("oh-my-nvim.plugins.lsp"),
+	--treesitter
+	require("oh-my-nvim.plugins.treesitter"),
+	require("oh-my-nvim.plugins.lsp"),
 
-    --formatter
-    require("oh-my-nvim.plugins.formatter"),
-    require("oh-my-nvim.plugins.linter"),
-    require("oh-my-nvim.plugins.trouble"),
+	--formatter
+	require("oh-my-nvim.plugins.formatter"),
+	require("oh-my-nvim.plugins.linter"),
+	require("oh-my-nvim.plugins.trouble"),
 
-    --db
-    require("oh-my-nvim.plugins.db"),
+	--db
+	require("oh-my-nvim.plugins.db"),
 
-    --undo-tree
-    require("oh-my-nvim.plugins.undo"),
+	--undo-tree
+	require("oh-my-nvim.plugins.undo"),
 
-    require("oh-my-nvim.plugins.cloack"),
-    require("oh-my-nvim.plugins.copilot"),
+	require("oh-my-nvim.plugins.cloack"),
+	require("oh-my-nvim.plugins.copilot"),
 
-    --theme
-    require("oh-my-nvim.plugins.theme"),
+	--theme
+	require("oh-my-nvim.plugins.theme"),
 
-    --general
-    require("oh-my-nvim.plugins.plugins"),
+	--general
+	require("oh-my-nvim.plugins.plugins"),
 
-    --general move plugin
-    require("oh-my-nvim.plugins.harpoon"),
-    require("oh-my-nvim.plugins.move"),
-    "dccsillag/magma-nvim"
-
+	--general move plugin
+	require("oh-my-nvim.plugins.harpoon"),
+	require("oh-my-nvim.plugins.move"),
+	"mg979/vim-visual-multi",
 }
--- .config/nvim/lua/oh-my-nvim/plugins/latex.lua
+++ .config/nvim/lua/oh-my-nvim/plugins/latex.lua
@@ -1,6 +1,20 @@
-return{
+return {
 	"lervag/vimtex",
-    config = function()
-        vim.g.vimtex_view_method = 'zathura'
-    end
+	config = function()
+		local g = vim.g
+		g.vimtex_view_method = "zathura"
+		-- g.vimtex_view_method = "zathura_simple"
+		g.vimtex_compiler_latexmk_engines = { ["_"] = "-xelatex" }
+		g.vimtex_compiler_latexmk = {
+			aux_dir = ".build",
+			-- out_dir = "",
+			options = {
+				"-shell-escape",
+				"-verbose",
+				"-file-line-error",
+				"-synctex=1",
+				"-interaction=nonstopmode",
+			},
+		}
+	end,
 }
