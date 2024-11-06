5aaf687f9f259fc09ac8c391eb6cf47a6a12fb82
Thomas Derudder <thomasderudder@sfr.fr>
2024-11-06 21:22:12 +0100

    :sparkles: feat: volume up down and brightness

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -6,8 +6,8 @@ static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int gappx = 5;
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static const char *fonts[] = {"CaskaydiaCoveNerdFont:size=10"};
+static const char dmenufont[] = "CaskaydiaCoveNerdFont:size=10";
 static const char col_second_background[] = "#FEDA99";
 static const char col_front_light[] = "#754C26";
 static const char col_front_dark[] = "#5A3410";
@@ -77,6 +77,7 @@ static const Layout layouts[] = {
   }
 
 /* commands */
+#include <X11/XF86keysym.h>
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
@@ -84,7 +85,7 @@ static const char *dmenucmd[] = {
     col_second_background, "-nf", col_front_light, "-sb", col_background, "-sf",
     col_front_dark,        NULL};
 static const char *termcmd[] = {"st", NULL};
-// static const char *firefox[] = {"firefox", NULL};
+// static constneo65 char *firefox[] = {"firefox", NULL};
 static const char *prtscrcmd[] = {"flameshot", "gui", NULL};
 static const char *clipmd[] = {"clipmenu", "-i",
                                "-fn",      dmenufont,
@@ -94,6 +95,16 @@ static const char *clipmd[] = {"clipmenu", "-i",
                                "-sf",      col_front_dark,
                                NULL};
 static const char *urlcmd[] = {"clipmenu-url", NULL};
+static const char *brupcmd[] = {"brightnessctl", "set", "10%+", NULL};
+static const char *brdowncmd[] = {"brightnessctl", "set", "10%-", NULL};
+static const char *volupcmd[] = {"pulsemixer", "--change-volume", "+10", NULL};
+static const char *voldowncmd[] = {"pulsemixer", "--change-volume", "-10",
+                                   NULL};
+static const char *mutecmd[] = {"pulsemixer", "--toggle-mute", NULL};
+static const char *mednextcmd[] = {"mpc", "next", NULL};
+static const char *medprevcmd[] = {"mpc", "prev", NULL};
+static const char *medplaypausecmd[] = {"txs", "music",  "&&",
+                                        "mpc", "toggle", NULL};
 
 #include "movestack.c"
 static const Key keys[] = {
@@ -104,6 +115,14 @@ static const Key keys[] = {
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_c, spawn, {.v = clipmd}},
     {MODKEY, XK_u, spawn, {.v = urlcmd}},
+    {0, XF86XK_AudioMute, spawn, {.v = mutecmd}},
+    {0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd}},
+    {0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd}},
+    {0, XF86XK_MonBrightnessUp, spawn, {.v = brupcmd}},
+    {0, XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd}},
+    {0, XF86XK_AudioNext, spawn, {.v = mednextcmd}},
+    {0, XF86XK_AudioPrev, spawn, {.v = medprevcmd}},
+    {0, XF86XK_AudioStop, spawn, {.v = medplaypausecmd}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
     {MODKEY, XK_i, incnmaster, {.i = +1}},
